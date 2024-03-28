be862d3e77134fea7f7ea815223f7e210f8d8000
leejaehyeon <ruitonau@gmail.com>
2024-03-28 15:56:52 +0900

    MySQL기본연결설정

--- app.js
+++ app.js
@@ -7,7 +7,26 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const mysql = require('promise-mysql');
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+// Google Cloud MySQL 데이터베이스 연결 설정
+const connection = mysql.createConnection({
+  host: '34.64.239.169', // Google Cloud SQL의 IP 주소
+  user: 'root', // 데이터베이스 사용자명
+  password: 'iJ8=dI%)}_0`X*|e', // 데이터베이스 암호
+  database: 'Review_dangil' // 연결할 데이터베이스명
+});
+
+// 데이터베이스 연결
+connection.then((conn) => {
+  console.log('Google Cloud MySQL 데이터베이스 연결 성공!');
+  startServer(); // 서버 시작 함수 호출
+}).catch((err) => {
+  console.error('데이터베이스 연결 오류:', err);
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
