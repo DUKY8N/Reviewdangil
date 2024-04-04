6bf41e66132598031a3eed80103b1276994daf0e
Dukyoon Im <DUKY8N@gmail.com>
2024-04-04 14:33:20 +0900

    login 접근 리다이렉트

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
+	res.redirect("/login");
+});
+
+router.get("/login", function (req, res, next) {
 	res.render("login");
 });
 
